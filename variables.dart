61471c2263184e6201325087ca72b0ce6f94dc73
Abu <abu@Abus-MacBook-Pro.local>
2025-05-10 21:38:03 -0500

    Commit

---
+++ variables.dart
@@ -0,0 +1,27 @@
+//Variables
+void main(){
+  //Variables
+
+  //Strings
+  //var name = 'Jon';
+  String name = 'Jon';
+
+  // Integer
+  //var x = 41;
+  int x = 41;
+
+  //Dynamic
+  dynamic firstName = 'Tim';
+
+  // Const and Final
+  const String fullName = 'Jon Smith';
+  final String nickName = 'Jon snow';
+
+  // declaring and not using
+  var myName;
+
+  print(myName);
+
+  myName = 'Jon';
+  print(myName);
+}
