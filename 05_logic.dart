3d878f73740ec7019b1004dab444abd2d94fbd8b
Abu <abu@Abus-MacBook-Pro.local>
2025-05-12 08:54:25 -0500

    push

---
+++ 05_logic.dart
@@ -0,0 +1,19 @@
+void main(){
+  // Logic - If, If/Else, If/Else If
+  var num = 50;
+  if (num == 5) {
+    print('Num is 5');
+  }
+  else if(num == 3){
+    print('The number is 3');
+
+
+  }else{
+    print('The number is not 3 or 5 it is $num');
+  }
+
+
+
+
+
+}
