7e37c0ed91a16fcc5c1c1781d06c259a937545de
Abu <abu@Abus-MacBook-Pro.local>
2025-05-11 23:07:38 -0500

    commit

--- maps.dart
+++ maps.dart
@@ -3,4 +3,28 @@ void main(){
   var toppings = {'John':'Pepperoni', 'Mary':'Cheese'};
   print(toppings);
   print(toppings['John']);
+
+// show values 
+print(toppings.keys);
+
+// show lenght
+print(toppings.length);
+
+//add something
+toppings['Tim']='sausage';
+print(toppings);
+
+//add many things
+toppings.addAll({'Tina':'Beacon', 'Steve':'Supereme'});
+print(toppings);
+
+// remove something
+toppings.remove('Steve');
+print(toppings);
+
+//remove everything
+toppings.clear();
+print(toppings);
+
+
 }
